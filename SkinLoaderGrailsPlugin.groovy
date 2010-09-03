508368405012c31cfee2855ac3e9dcaebc447e10
acampbell3000 <acampbell3000@googlemail.com>
2010-09-03 21:34:13 +0100

    Grails upgrade.

--- SkinLoaderGrailsPlugin.groovy
+++ SkinLoaderGrailsPlugin.groovy
@@ -54,31 +54,4 @@ To install, run the following command:
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/skin-loader"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
