196821afec7b9844918903114dae4eca362ff0df
Anthony Campbell <acampbell3000@googlemail.com>
2011-09-04 22:53:39 +0100

    Introduction of the release plug-in, required to release plugins with 2.0.0.M1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,6 +37,7 @@ grails.project.dependency.resolution = {
         compile ":hibernate:$grailsVersion"
         compile ":jquery:1.6.1.1"
         compile ":resources:1.0.2"
+        compile ":release:1.0.0.RC3"
 
         build ":tomcat:$grailsVersion"
     }
