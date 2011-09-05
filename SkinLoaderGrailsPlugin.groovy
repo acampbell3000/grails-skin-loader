a5232bafc9fd9bb8906d7fdd40cf1a399fcfd575
Anthony Campbell <acampbell3000@googlemail.com>
2011-09-05 01:35:43 +0100

    Update to version 1.0.10 in preparation of next release.

--- SkinLoaderGrailsPlugin.groovy
+++ SkinLoaderGrailsPlugin.groovy
@@ -25,7 +25,7 @@
  */
 class SkinLoaderGrailsPlugin {
     // the plugin version
-    def version = "1.0.9"
+    def version = "1.0.10"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.0.M1"
     // the other plugins this plugin depends on
